3b1f7246df906949d5065d0a4641c295a0326326
김준희 <shadd3338@gmail.com>
2026-03-31 11:14:01 +0900

    #48 [MOD] 어드민 레이아웃 배경색 black으로 변경

--- src/layouts/admin/AdminLayout.tsx
+++ src/layouts/admin/AdminLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 const AdminLayout: React.FC = () => {
   return (
-    <div className="flex h-screen bg-background">
+    <div className="flex h-screen bg-black">
       <Sidebar />
       <main className="flex-1 overflow-y-auto scrollbar-hide min-w-fit">
         <Outlet />
--- src/pages/admin/seminar-live/Attendance.tsx
+++ src/pages/admin/seminar-live/Attendance.tsx
@@ -1,11 +1,10 @@
+import ApplicantsList from '../../../components/admin/attendance/SeminarList';
+
 const Attendance = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center">
-      <h1 className="heading-2-semibold text-green-400 mb-8">페이지 준비 중입니다!</h1>
-      <p className="body-2-medium text-grey-400">
-        출석 관리 페이지를 개발 중이에요.
-        <br />곧 만나보실 수 있습니다!
-      </p>
+    <div className="mx-60 my-60">
+      <h1 className="text-white heading-1-bold mb-12">세미나 출석 관리</h1>
+      <ApplicantsList />
     </div>
   );
 };

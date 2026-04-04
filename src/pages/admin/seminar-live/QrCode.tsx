807864424d57bb11238ec09c939534b6d55e62f7
김준희 <shadd3338@gmail.com>
2026-04-04 19:00:56 +0900

    #31 [FEAT] QR 코드 스캔 시 출석 인증 페이지로 이동하도록 변경

--- src/pages/admin/seminar-live/QrCode.tsx
+++ src/pages/admin/seminar-live/QrCode.tsx
@@ -1,15 +1,16 @@
 import { useParams } from 'react-router-dom';
+import { QRCodeSVG } from 'qrcode.react';
 import { useSeminarNums } from '../../../hooks/Applicants/useSeminarNums';
-import { useSeminarQr } from '../../../hooks/Applicants/useSeminarQr';
 
 const QrCode = () => {
   const { seminarId } = useParams<{ seminarId: string }>();
   const { data: seminarNumsData } = useSeminarNums();
-  const { data: qrData } = useSeminarQr(Number(seminarId));
 
   const seminar = seminarNumsData?.result?.find(
     (item) => item.seminarId === Number(seminarId),
   );
+
+  const verificationUrl = `${window.location.origin}/seminar/live/verification`;
 
   return (
     <div className="mx-60 my-60">
@@ -17,9 +18,7 @@ const QrCode = () => {
         제 {seminar?.seminarNum}회 Devtalk 세미나 QR
       </h1>
       <div className="flex justify-center items-center mt-60">
-        {qrData?.result && (
-          <img src={qrData.result} alt="QR 코드" style={{ width: 500, height: 500 }} />
-        )}
+        <QRCodeSVG value={verificationUrl} size={500} />
       </div>
     </div>
   );

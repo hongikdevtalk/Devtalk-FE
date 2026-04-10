c5415c171b34f0a3da68bdf4305faea7ba25bad8
김준희 <shadd3338@gmail.com>
2026-04-11 02:16:47 +0900

    #51 [FEAT] 세미나 상세 페이지 플로팅 버튼 개선

--- src/components/common/Cta.tsx
+++ src/components/common/Cta.tsx
@@ -1,39 +1,20 @@
-import { useShowSeminar } from '../../contexts/ShowSeminarContext';
-import { useGetUserSeminar } from '../../hooks/userMainPage/useSeminar';
-import { Button } from '../Button/Button';
-
 type CtaProps = {
-  bodyText: string;
+  bodyText?: string;
   buttonText: string;
   onClick: () => void;
-  isActive?: boolean;
 };
 
-const Cta = ({ bodyText, buttonText, onClick, isActive }: CtaProps) => {
-  const { seminarId } = useShowSeminar();
-  const { data: seminar } = useGetUserSeminar(seminarId ?? 1);
-
-  const extractDate = (dateString?: string) => dateString?.split(' ')[0] ?? '';
-
+const Cta = ({ bodyText, buttonText, onClick }: CtaProps) => {
   return (
-    <div
-      className={`w-[375px] ${isActive ? 'h-[128px]' : 'h-[152px]'} flex flex-col justify-center items-center gap-16 px-20 pt-20 pb-[24px] button-bg`}
-    >
-      {isActive ? (
-        <div className="body-2-semibold text-grey-100">{bodyText}</div>
-      ) : (
-        <div className="flex flex-col items-center gap-4 w-[256px] h-[44px] body-2-semibold text-grey-100">
-          {bodyText}
-          <div className="whitespace-nowrap">
-            신청 기간은{' '}
-            <span className="text-gradient">
-              {`${extractDate(seminar?.result?.startDate)} ~ ${extractDate(seminar?.result?.endDate)}`}
-            </span>{' '}
-            입니다
-          </div>
-        </div>
-      )}
-      <Button variant="home" text={buttonText} onClick={onClick} />
+    <div className="w-[375px] flex flex-col justify-center items-center gap-16 px-20 pt-20 pb-[24px]">
+      {bodyText && <div className="body-2-semibold text-grey-100">{bodyText}</div>}
+      <button
+        onClick={onClick}
+        className="flex h-[56px] px-24 justify-center items-center gap-[10px] self-stretch rounded-[10px] text-white text-[20px] font-semibold leading-normal cursor-pointer"
+        style={{ background: 'radial-gradient(557.08% 171.17% at 74.62% 100%, #BDF548 0%, #4EABB5 100%)' }}
+      >
+        {buttonText}
+      </button>
     </div>
   );
 };
